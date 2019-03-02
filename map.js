f0af4d20a7b96bfbd5881a1cb43b4abffa5222bc
ch3341 <ch3341@columbia.edu>
2019-03-01 23:08:54 -0500

    Puerto Rico Map

--- map.js
+++ map.js
@@ -8,9 +8,9 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY2gzMzQxIiwiYSI6ImNqc3FzZ2JkZDFiMnEzeW14bzVob
 
 let map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10',
-    center: [-73.96024, 40.80877],
-    zoom: 12
+    style: 'mapbox://styles/mapbox/navigation-guidance-day-v2',
+    center: [-66.53300, 18.21456],
+    zoom: 8
 })
 
 // create an instance of NavigationControl
@@ -54,3 +54,52 @@ geolocate.on('geolocate', function(event) {
     // format lng lat values and display them on our 'info' element
     document.getElementById('info').innerHTML = lng.toFixed(5) + "," + lat.toFixed(5)
 })
+map.on('click', function(event) {
+
+    let lng = event.lngLat.lng
+    let lat = event.lngLat.lat
+
+    console.log("clicked:", lng, lat)
+
+    document.getElementById('info').innerHTML = lng.toFixed(5) + "," + lat.toFixed(5)
+})
+let marker = new mapboxgl.Marker()
+marker.setLngLat([-66.53300,18.21456])
+marker.addTo(map)
+let popup = new mapboxgl.Popup()
+popup.setHTML('Places I Visited Puerto Rico 2014')
+marker.setPopup(popup)
+let data = [
+    {
+        location: [-66.06005,18.43277],
+        content: 'My journey begins in Old San Juan'
+    },
+    {
+        location: [-65.78034,18.31821],
+        content: 'El Yunque is a quick drive from Luquillo'
+    },
+    {
+        location: [-66.61393,18.01074],
+        content: 'Ponce was not deserted in 2014'
+    },
+    {
+        location: [-67.21417,17.98503],
+        content: 'My favorite beaches are in Cabo Rojo'
+    },
+    {
+        location: [-66.71175,18.45527],
+        content: 'Arecibo is another popular town'
+    },
+    ]
+    data.forEach(function(d) {
+
+    let marker = new mapboxgl.Marker()    
+    marker.setLngLat(d.location)
+    marker.addTo(map)  
+
+    let popup = new mapboxgl.Popup()
+    popup.setHTML(d.content)
+    marker.setPopup(popup)
+
+})
+
